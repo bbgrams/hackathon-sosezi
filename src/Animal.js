77eddf9806aa3d58df4e99a1b7ff97db2be74259
Ahn sena <serendipity.sena@gmail.com>
2018-11-16 17:56:50 +0900

    Animal.js and Baby.js

--- src/Animal.js
+++ src/Animal.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import api from './api'
 import Baby from './Baby'
+
 export default class Animal extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      babys: [],
+      info: false
+    }
+  }
+
+  async componentDidMount(){
+    const {data: babys} = await api.get('/babys')
+
+    this.setState({
+      babys
+    })
+  }
+
   render(){
     return (
     <div className="animal-wrap">
-        <Baby />
+        {this.state.babys.map(baby => (
+          <dl className="baby-board" key={baby.id}>
+            <dt className="baby-name">{baby.name}</dt>
+            <Baby babyId={baby.id}/>
+          </dl>
+        ))}
     </div>
     )
   }
